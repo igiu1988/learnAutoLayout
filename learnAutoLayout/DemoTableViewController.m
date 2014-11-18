ce579d0615da7552c4f500861db0dc2d747106ba
Nicolas wang <igiuify@gmail.com>
2014-11-18 08:30:28 +0800

    save

--- learnAutoLayout/DemoTableViewController.m
+++ learnAutoLayout/DemoTableViewController.m
@@ -20,11 +20,11 @@
     
     TableHeader *header = [[NSBundle mainBundle] loadNibNamed:@"TableHeader" owner:nil options:nil][0];
     header.contentLabel.text = @"How am I supposed to configure programmatically (and in which method) a UILabel whose height depends on its text? I've been trying to set it up using a combination of Storyboard and code, but to no avail. Everyone recommends sizeToFit while setting lineBreakMode and numberOfLines. However, no matter if I put that code in viewDidLoad:, viewDidAppear:";
-////    
-//    [header setNeedsLayout];
-//    [header layoutIfNeeded];
+    NSLog(@"table width: %f",self.tableView.width);
     
-    CGFloat height = [header systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+    [header setNeedsLayout];
+    [header layoutIfNeeded];
+    CGFloat height = [header systemLayoutSizeFittingSize:CGSizeMake(self.tableView.width, 0)].height;
     CGRect frame = header.frame;
     frame.size.height = height;
     header.frame = frame;
@@ -36,6 +36,21 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)headerChangeAction:(id)sender {
+    TableHeader *header = (TableHeader *)self.tableView.tableHeaderView;
+    
+    header.contentLabel.text = @"How am I supposed to configure programmatically (and in which method) a UILabel whose height depends on its text? I've been trying to set it up using a combination of Storyboard and code, but to no avail. ";
+    ////
+    //    [header setNeedsLayout];
+    //    [header layoutIfNeeded];
+    
+    CGFloat height = [header systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+    CGRect frame = header.frame;
+    frame.size.height = height;
+    header.frame = frame;
+    
+    self.tableView.tableHeaderView = header;
 }
 
 @end
